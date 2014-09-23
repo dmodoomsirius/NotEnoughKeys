39e108a22658aa2cfc7b777c992ea837dc144829
Parker Young <Parker8283@users.noreply.github.com>
2014-09-22 20:19:15 -0400

    Fixed Ticker

--- src/main/java/okushama/notenoughkeys/NotEnoughKeys.java
+++ src/main/java/okushama/notenoughkeys/NotEnoughKeys.java
@@ -1,8 +1,11 @@
 package okushama.notenoughkeys;
 
 import java.util.ArrayList;
-import java.util.logging.Logger;
+import java.util.Collections;
 
+import org.apache.logging.log4j.Logger;
+
+import cpw.mods.fml.common.FMLCommonHandler;
 import okushama.notenoughkeys.console.Console;
 import okushama.notenoughkeys.keys.Binds;
 import okushama.notenoughkeys.keys.KeybindTracker;
@@ -17,13 +20,12 @@ import cpw.mods.fml.common.ModContainer;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.registry.TickRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 @Mod(modid = NotEnoughKeys.modid, name = NotEnoughKeys.name, version = NotEnoughKeys.version)
 public class NotEnoughKeys {
-	public static final String modid = "notenoughkeys", name = "Not Enough Keys", version = "0.0.4";
+	public static final String modid = "notenoughkeys", name = "Not Enough Keys", version = "@MOD_VERSION@";
 
 	public static Logger logger;
 	public static Console console = new Console();
@@ -34,15 +36,14 @@ public class NotEnoughKeys {
 		logger = e.getModLog();
 		KeybindTracker.modKeybinds.put("All", new ArrayList<KeyBinding>());
 		ArrayList<KeyBinding> vanillaKeys = new ArrayList<KeyBinding>();
-		for (KeyBinding kb : Minecraft.getMinecraft().gameSettings.keyBindings)
-			vanillaKeys.add(kb);
+        Collections.addAll(vanillaKeys, Minecraft.getMinecraft().gameSettings.keyBindings);
 		KeybindTracker.modKeybinds.put("Minecraft", vanillaKeys);
 	}
 
 	@EventHandler
 	@SideOnly(Side.CLIENT)
 	public static void init(FMLInitializationEvent e) {
-		TickRegistry.registerTickHandler(new Ticker(), Side.CLIENT);
+        FMLCommonHandler.instance().bus().register(new Ticker());
 		ClientRegistry.registerKeyBinding(new Keybinds());
 		Binds.init();
 	}
--- src/main/java/okushama/notenoughkeys/Ticker.java
+++ src/main/java/okushama/notenoughkeys/Ticker.java
@@ -1,53 +1,23 @@
 package okushama.notenoughkeys;
 
-import java.util.EnumSet;
-import java.util.logging.Level;
-
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
 import okushama.notenoughkeys.gui.GuiControlsOverride;
 import okushama.notenoughkeys.gui.GuiKeybindsMenu;
 import okushama.notenoughkeys.keys.Binds;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiControls;
-import cpw.mods.fml.common.ITickHandler;
-import cpw.mods.fml.common.TickType;
 
-public class Ticker implements ITickHandler {
-	public boolean liteloaderExists = true;
+public class Ticker {
 
-	@Override
-	public void tickStart(EnumSet<TickType> type, Object... tickData) {
-		if (type.equals(EnumSet.of(TickType.CLIENT))) {
-			Binds.tick();
-			if (liteloaderExists) {
-				try {
-					Class liteloadergui = Class.forName("com.mumfrey.liteloader.gui.GuiControlsPaginated");
-					if (liteloadergui != null) {
-						if (Minecraft.getMinecraft().currentScreen != null && Minecraft.getMinecraft().currentScreen.getClass().equals(liteloadergui)) {
-							Minecraft.getMinecraft().displayGuiScreen(new GuiKeybindsMenu());
-							NotEnoughKeys.logger.log(Level.INFO, "Replaced the instance of LiteLoader's controls gui!");
-						}
-					}
-				} catch (ClassNotFoundException e) {
-					liteloaderExists = false;
-				}
-			}
-			if (Minecraft.getMinecraft().currentScreen != null && Minecraft.getMinecraft().currentScreen instanceof GuiControls && !(Minecraft.getMinecraft().currentScreen instanceof GuiControlsOverride)) {
-				Minecraft.getMinecraft().displayGuiScreen(new GuiKeybindsMenu());
-				NotEnoughKeys.logger.log(Level.INFO, "Replaced the instance of Minecraft controls gui!");
-			}
-		}
-	}
-
-	@Override
-	public void tickEnd(EnumSet<TickType> type, Object... tickData) {}
-
-	@Override
-	public EnumSet<TickType> ticks() {
-		return EnumSet.of(TickType.CLIENT);
-	}
-
-	@Override
-	public String getLabel() {
-		return "Not Enough Ticker";
-	}
+    @SubscribeEvent
+    public void onClientTick(TickEvent.ClientTickEvent event) {
+        if(event.phase == TickEvent.Phase.START) {
+            Binds.tick();
+            if (Minecraft.getMinecraft().currentScreen != null && Minecraft.getMinecraft().currentScreen instanceof GuiControls && !(Minecraft.getMinecraft().currentScreen instanceof GuiControlsOverride)) {
+                Minecraft.getMinecraft().displayGuiScreen(new GuiKeybindsMenu());
+                NotEnoughKeys.logger.info("Replaced the instance of Minecraft controls gui!");
+            }
+        }
+    }
 }
