915bf2e37aed55c16a34bc5a23d0c0a56b0204bf
TheTemporatlist <TheTemportalist@users.noreply.github.com>
2014-11-09 15:18:06 -0500

    Javadoc for API

--- src/main/java/modwarriors/notenoughkeys/api/Api.java
+++ src/main/java/modwarriors/notenoughkeys/api/Api.java
@@ -1,16 +1,25 @@
 package modwarriors.notenoughkeys.api;
 
 import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 /**
  * @author TheTemportalist
  */
+@SideOnly(Side.CLIENT)
 public class Api {
 
 	public static boolean isLoaded() {
 		return Loader.isModLoaded("notenoughkeys");
 	}
 
+	/**
+	 * Registers a mod's keys with NEK
+	 *
+	 * @param modname        The NAME of the mod registering the key
+	 * @param keyDecriptions A String[] (Array[String]) of the key descriptions. i.e. new String[]{"key.hotbar1"}
+	 */
 	public static void registerMod(String modname, String[] keyDecriptions) {
 		try {
 			Class.forName("modwarriors.notenoughkeys.keys.KeyBindTracker").getMethod(
